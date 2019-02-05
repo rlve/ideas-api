32e96aef48ad1d0c9cbdacdab60d16851c4f16d5
rlve <radioslav@gmail.com>
2019-02-05 22:37:40 +0100

    Idea CRUD -  unit tests not working

--- src/idea/idea.controller.spec.ts
+++ src/idea/idea.controller.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IdeaController } from './idea.controller';
+import { IdeaService } from './idea.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IdeaEntity } from './idea.entity';
 
 describe('Idea Controller', () => {
   let controller: IdeaController;
+  let service: IdeaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [IdeaController],
+      providers: [IdeaService],
     }).compile();
 
+    service = module.get<IdeaService>(IdeaService);
     controller = module.get<IdeaController>(IdeaController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { IdeaService } from './idea.service';
+import { IdeaDTO } from './idea.dto';
 
 @Controller('idea')
-export class IdeaController {}
+export class IdeaController {
+  constructor(private ideaService: IdeaService) {}
+
+  @Get()
+  showAllIdeas() {
+    return this.ideaService.showAll();
+  }
+
+  @Post()
+  createIdea(@Body() data: IdeaDTO) {
+    return this.ideaService.create(data);
+  }
+
+  @Get(':id')
+  showIdea(@Param('id') id: string) {
+    return this.ideaService.read(id);
+  }
+
+  @Put(':id')
+  updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDTO>) {
+    return this.ideaService.update(id, data);
+  }
+
+  @Delete(':id')
+  deleteIdea(@Param('id') id: string) {
+    return this.ideaService.delete(id);
+  }
+}
