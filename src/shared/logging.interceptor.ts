00696581baf773dfaa4c44622ec715f418ca95b5
rlve <radioslav@gmail.com>
2019-02-07 21:59:30 +0100

    Logging interceptor

---
+++ src/shared/logging.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  Logger,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    call$: Observable<any>,
+  ): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+    const { method, url } = req;
+    const now = Date.now();
+
+    return call$.pipe(
+      tap(() =>
+        Logger.log(
+          `${method} ${url} ${Date.now() - now}ms`,
+          context.getClass().name,
+        ),
+      ),
+    );
+  }
+}
