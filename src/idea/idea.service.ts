516a3bcf4a72d76a78d2d49a22739cd2a6d89313
rlve <radioslav@gmail.com>
2019-02-16 15:39:44 +0100

    idea.service: add validateId and validate if foundOne

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { isUUID } from 'validator';
 
 import { IdeaEntity } from './idea.entity';
 import { IdeaDTO } from './idea.dto';
 
+const validateId = (id: string): boolean => {
+  const result = isUUID(id);
+
+  if (!result) {
+    throw new HttpException(
+      'Id must be a valid UUID',
+      HttpStatus.NOT_ACCEPTABLE,
+    );
+  }
+
+  return result;
+};
+
 @Injectable()
 export class IdeaService {
-  constructor(@InjectRepository(IdeaEntity) private ideaRepository: Repository<IdeaEntity>) {}
+  constructor(
+    @InjectRepository(IdeaEntity)
+    private ideaRepository: Repository<IdeaEntity>,
+  ) {}
 
   async showAll() {
     return await this.ideaRepository.find();
@@ -20,16 +37,42 @@ export class IdeaService {
   }
 
   async read(id: string) {
-    return await this.ideaRepository.findOne({where: {id}});
+    let idea: IdeaDTO | undefined;
+    if (validateId(id)) {
+      idea = await this.ideaRepository.findOne({ where: { id } });
+
+      if (!idea) {
+        throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+      }
+    }
+    return idea;
   }
 
   async update(id: string, data: Partial<IdeaDTO>) {
+    let idea: IdeaDTO | undefined;
+    if (validateId(id)) {
+      idea = await this.ideaRepository.findOne({ where: { id } });
+
+      if (!idea) {
+        throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+      }
+    }
+
     await this.ideaRepository.update({ id }, data);
-    return await this.ideaRepository.findOne({where: {id}});
+    return await idea;
   }
 
   async delete(id: string) {
-    await this.ideaRepository.delete({id});
-    return { deleted: true };
+    let idea: IdeaDTO | undefined;
+    if (validateId(id)) {
+      idea = await this.ideaRepository.findOne({ where: { id } });
+
+      if (!idea) {
+        throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+      }
+    }
+
+    await this.ideaRepository.delete({ id });
+    return idea;
   }
 }
