262254df89f23f182b18f655985e7165eaad484c
rlve <radioslav@gmail.com>
2019-04-02 22:30:49 +0200

    enable corss

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // angular
+      'http://localhost:3000', // react
+      'http://localhost:8081', // react-native
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   Logger.log(`Server running on http//localhost:${port}`, 'Bootstrap');
